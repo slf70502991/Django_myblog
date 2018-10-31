d50e92181e079c316f10d1d5768c1bec99491647
slf70502991 <slf70502991@hotmail.com>
2018-10-31 15:56:25 +0800

    6

--- myblog/settings/production.py
+++ myblog/settings/production.py
@@ -21,13 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'go@k-wum^=c*gn*-7&hiea0h9t*o@zlc18hg!p03*4h=w(!hxr'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'go@k-wum^=c*gn*-7&hiea0h9t*o@zlc18hg!p03*4h=w(!hxr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['whispering-harbor-59600.herokuapp.com']
 
 
 # Application definition
@@ -83,6 +83,12 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# add this
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 # Password validation
